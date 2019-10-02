201bfab22eea9d1e713ff39b2320fae4e9830c96
felipe-barrera <juanf.barrerar@utadeo.edu.co>
2019-10-01 19:56:03 -0500

    primer commit

--- Fixie/app/src/main/java/com/lock/fixie/Interfaces/IComunicaFragments.java
+++
@@ -1,9 +0,0 @@
-package com.lock.fixie.Interfaces;
-
-public interface IComunicaFragments {
-
-    public void Imagenes();
-    public void Registros();
-    public void Perfil();
-    public void Ajustes();
-}
